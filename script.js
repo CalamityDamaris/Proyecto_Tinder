c22033f16d5dad3502f57cf1737c19a33e292bb1
Facem404 <f_maggi@outlook.com>
2022-08-09 09:55:36 -0300

    Header y js

---
+++ script.js
@@ -0,0 +1,16 @@
+const arrayLista = ["https://robohash.org/moleng?size=50x50&setstiaeetsit.p=set1","https://robohash.org/quilaudantiumea.png?size=50x50&set=set1","https://robohash.org/mollitiaquisquammodi.png?size=50x50&set=set1","https://robohash.org/nihilautvoluptatem.png?size=50x50&set=set1","https://robohash.org/etquamaut.png?size=50x50&set=set1"]
+
+const arrayListaInfo = ["Nombre","Hola! como estas?"]
+
+const robotin = {
+    imagen:"https://mir-s3-cdn-cf.behance.net/project_modules/fs/0b6413117868287.607e24623278b.jpg",
+    nombre: "ROBOTIN",
+    edad: 202,
+    gusto1: "Cantar",
+    gusto2: "Bailar",
+    gusto3: "Programar",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem molestiae delectus corporis eaque ut inventore ullam perspiciatis quas, numquam molestias rerum libero pariatur laboriosam quisquam incidunt ducimus, deserunt reprehenderit sint.",
+}
+
+let imagen1 = arrayLista[0]
+$("imagen1").attr("src",imagen1)
