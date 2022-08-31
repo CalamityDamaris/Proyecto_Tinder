cf4efe76399db676bb74dc095a4fee270da0fa32
Facem404 <f_maggi@outlook.com>
2022-08-31 14:03:30 -0300

    Pagina login mejorada

--- public/script/vue.js
+++ public/script/vue.js
@@ -1,6 +1,27 @@
 const app = new Vue({
     el: ".app",
     data: {
-        titulo: "Hola mundo con VUE"
+        titulo: "Hola mundo con VUE",
+        registro: "Haz click aqui para registrarte!",
+        nuevoNombre: "",
+        nuevoApellido:"",
+        nuevoCorreo:"",
+        nuevaContraseña:"",
+        login: "Iniciar Sesión",
+        reg: "Registrarse"
+    },
+    methods:{
+        desplegarLogin(){
+            const div_selector = document.getElementById("selector_login_registro")
+            div_selector.style.display = "none"
+            const formulario_login = document.getElementById("login")
+            formulario_login.style.display = "flex"
+        },
+        desplegarRegistro(){
+            const div_selector = document.getElementById("selector_login_registro")
+            div_selector.style.display = "none"
+            const formulario_registro = document.getElementById("registro")
+            formulario_registro.style.display = "flex"
+        }
     }
 })
